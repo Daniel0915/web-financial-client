a574430ee93d67508ad5e0ecd7ec1255bbbf62e1
Daniel0915 <ufo9363@gmail.com>
2025-01-21 21:13:12 +0900

    indexedDB 설정

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,6 +8,60 @@ import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 import { ChannelTypeEnum } from "@/data/enum/ChannelTypeEnum";
 
 
+// TODO : index db
+if (!window.indexedDB) {
+    window.alert("121");
+}
+
+// This is what our customer data looks like.
+const customerData = [
+    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+];
+
+const dbName = "the_name";
+
+var request = indexedDB.open(dbName, 5);
+
+request.onerror = function (event) {
+    // Handle errors.
+};
+request.onupgradeneeded = function (event) {
+    var db = event.target.result;
+
+    // Create an objectStore to hold information about our customers. We're
+    // going to use "ssn" as our key path because it's guaranteed to be
+    // unique - or at least that's what I was told during the kickoff meeting.
+    var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+    // Create an index to search customers by name. We may have duplicates
+    // so we can't use a unique index.
+    objectStore.createIndex("name", "name", { unique: false });
+
+    // Create an index to search customers by email. We want to ensure that
+    // no two customers have the same email, so use a unique index.
+    objectStore.createIndex("email", "email", { unique: true });
+
+    // Use transaction oncomplete to make sure the objectStore creation is
+    // finished before adding data into it.
+    objectStore.transaction.oncomplete = function (event) {
+        // Store values in the newly created objectStore.
+        var customerObjectStore = db.transaction("customers", "readwrite")
+                                    .objectStore("customers");
+
+        customerData.forEach(function (customer) {
+            customerObjectStore.add(customer);
+        });
+    };
+};
+
+
+
+
+// TODO : index db
+
+
+
 // EventSource 에서 받은 메시지를 저장할 상태
 const messages = ref([]); // 실시간 메시지를 담는 배열
 
