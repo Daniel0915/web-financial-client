c2438dc8f48020eb0c2ce21c58ee945aa2e6c061
Daniel0915 <ufo9363@gmail.com>
2025-02-09 23:40:48 +0900

    1. 거래량, 평단가, 보유주식 필터 추가

--- src/views/LargeHoldings.vue
+++ src/views/LargeHoldings.vue
@@ -9,6 +9,8 @@ const page = ref(0);
 const totalRecords = ref(0);
 const rows = ref(10);
 const tradeDateRange = ref(null);
+const sortField = ref('tradeDt');
+const sortOrder = ref(true); // 내림차순 default
 
 const selectList = ref([
     { name: "검색어 선택", code: "" },
@@ -18,6 +20,13 @@ const selectList = ref([
 ]);
 const selected = ref(selectList.value[0]);
 const inputText = ref(null);
+
+const onSort = (event) => {
+    sortField.value = event.sortField;
+    sortOrder.value = event.sortOrder;
+    let param = getSearchParam(0, selected.value, inputText.value, tradeDateRange.value, sortField.value, sortOrder.value);
+    searchData(param);
+};
 
 const isButtonEnabled = computed(() => {
     // init selected value case
@@ -53,29 +62,37 @@ function searchData(params) {
         largeHoldingsDetailList.value.forEach((largeHoldingsDetail) => {
             largeHoldingsDetail.tradeDt = largeHoldingsDetail.tradeDt.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
             largeHoldingsDetail.url = `https://dart.fss.or.kr/dsaf001/main.do?rcpNo=${largeHoldingsDetail.rceptNo}`;
+
         });
     });
 }
 
-function searchBtn(selected, inputText, tradeDateRange) {
-    const param = getSearchParam(0, selected, inputText, tradeDateRange);
-    this.searchData(param);
+function formatAccountingNumber(number) {
+    const absNumber = Math.abs(number).toLocaleString('en-US'); // 천 단위 콤마 추가
+
+    // 음수는 괄호로 표시, 양수는 그대로 표시
+    return number < 0 ? `(${absNumber})` : absNumber;
 }
 
-function onPageChange(event, selected, inputText, tradeDateRange) {
-    const param = getSearchParam(event.page, selected, inputText, tradeDateRange);
+function searchBtn(selected, inputText, tradeDateRange, sortField, sortOrder) {
+    const param = getSearchParam(0, selected, inputText, tradeDateRange, sortField, sortOrder);
     searchData(param);
 }
 
-function getSearchParam(inputPage, selected, inputText, tradeDateRange) {
+function onPageChange(event, selected, inputText, tradeDateRange, sortField, sortOrder) {
+    const param = getSearchParam(event.page, selected, inputText, tradeDateRange, sortField, sortOrder);
+    searchData(param);
+}
+
+function getSearchParam(inputPage, selected, inputText, tradeDateRange, orderColumn, sortOrder) {
     page.value = inputPage;
     const tradeDtGoe = !tradeDateRange ? null : formatDate(tradeDateRange[0]);
     const tradeDtLoe = !tradeDateRange ? null : formatDate(tradeDateRange[1]);
     const key = selected.code;
 
     return {
-        orderColumn: "tradeDt",
-        isDescending: true,
+        orderColumn: orderColumn ?? "tradeDt",
+        isDescending: sortOrder === null || sortOrder === undefined || sortOrder === '' ? true : (sortOrder === -1),
         page: page.value,
         size: rows.value,
         ...(key && { [key]: inputText }),
@@ -91,7 +108,11 @@ function formatDate(date) {
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
     return `${year}${month}${day}`;
-};
+}
+
+function windowOpen(url) {
+    window.open(url, '_blank');
+}
 
 
 </script>
@@ -105,6 +126,8 @@ function formatDate(date) {
             dataKey="seq"
             :rowHover="true"
             :loading="loading1"
+            removableSort
+            @sort="onSort"
             showGridlines
         >
             <template #header>
@@ -122,7 +145,7 @@ function formatDate(date) {
 
                         <Select v-model="selected" :options="selectList" optionLabel="name" placeholder="검색어 선택" />
                         <InputText :placeholder="selected.code === '' ? '검색어 선택해주세요.' : '검색어 입력해주세요.'" v-model="inputText" :disabled="selected.code === ''"/>
-                        <Button label="검색" @click="searchBtn(selected, inputText, tradeDateRange)" :disabled="!isButtonEnabled" />
+                        <Button label="검색" @click="searchBtn(selected, inputText, tradeDateRange, sortField, sortOrder)" :disabled="!isButtonEnabled" />
                     </InputGroup>
 
                 </div>
@@ -133,17 +156,38 @@ function formatDate(date) {
             <Column field="largeHoldingsName" header="내부자 이름" style="min-width: 12rem"></Column>
             <Column field="tradeReason" header="매매 이유" style="min-width: 12rem"></Column>
             <Column field="stockType" header="증권 종류" style="min-width: 12rem"></Column>
-            <Column field="tradeDt" header="거래 날짜" style="min-width: 12rem"></Column>
-            <Column field="changeStockAmount" header="거래량" style="min-width: 12rem"></Column>
-            <Column field="unitStockPrice" header="평단가" style="min-width: 12rem"></Column>
-            <Column field="afterStockAmount" header="보유주식" style="min-width: 12rem"></Column>
-            <Column field="url" header="URL" style="min-width: 12rem"></Column>
+            <Column field="tradeDt" header="거래 날짜" :sortable="true" style="min-width: 12rem"></Column>
+            <Column field="changeStockAmount" header="거래량" :sortable="true" style="min-width: 12rem">
+                <template #body="slotProps">
+                    {{ formatAccountingNumber(slotProps.data.changeStockAmount) }}주
+                </template>
+            </Column>
+            <Column field="unitStockPrice" header="평단가" :sortable="true" style="min-width: 12rem">
+                <template #body="slotProps">
+                    {{ formatAccountingNumber(slotProps.data.unitStockPrice) }}원
+                </template>
+            </Column>
+            <Column field="afterStockAmount" header="보유주식" :sortable="true" style="min-width: 12rem">
+                <template #body="slotProps">
+                    {{ formatAccountingNumber(slotProps.data.afterStockAmount) }}주
+                </template>
+            </Column>
+            <Column field="url" header="URL" style="min-width: 12rem">
+                <template #body="slotProps">
+                    <Button
+                        label="Dart 공시 URL"
+                        icon="pi pi-external-link"
+                        class="p-button-outlined p-button-sm"
+                        @click="windowOpen(slotProps.data.url)"
+                    />
+                </template>
+            </Column>
         </DataTable>
         <Paginator
             :rows="rows"
             :totalRecords="totalRecords"
             :first="page * rows"
-            @page="onPageChange($event, selected, inputText, tradeDateRange)"
+            @page="onPageChange($event, selected, inputText, tradeDateRange, sortField, sortOrder)"
         />
     </div>
 </template>
